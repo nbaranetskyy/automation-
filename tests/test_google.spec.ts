fa1ea06ea6a2035b9f734f4b460f240bb30b14d8
Nazariy Baranetskyy <nazariybaranetskyy@mac.mynetworksettings.com>
2025-10-01 21:06:52 -0400

    Clean up unused code

--- tests/test_google.spec.ts
+++ tests/test_google.spec.ts
@@ -1,9 +1,5 @@
 import { test, expect } from '@playwright/test';
-
 test('check test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright'); 
-//  const title = await page.title();
-//   console.log("ACTUAL TITLE:", title);
-//   await expect(page).toHaveTitle("Hello");
 });
