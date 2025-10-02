f3fbd000b534b880b8a72a5110798bbb8cdba304
Nazariy Baranetskyy <nazariybaranetskyy@mac.mynetworksettings.com>
2025-10-01 21:16:03 -0400

    test-commit

--- tests/test_google.spec.ts
+++ tests/test_google.spec.ts
@@ -2,4 +2,5 @@ import { test, expect } from '@playwright/test';
 test('check test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright'); 
+  // test-test
 });
