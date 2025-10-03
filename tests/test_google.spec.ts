deb3df918b1a4688bcb1aa2318e789fbf476bd0f
Nazariy Baranetskyy <nazariybaranetskyy@mac.mynetworksettings.com>
2025-10-03 19:54:30 -0400

    Test Friday2

--- tests/test_google.spec.ts
+++ tests/test_google.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 test('check test', async ({ page }) => {
-  await page.goto('https://playwright.dev/'); // Friday FunDay
+  await page.goto('https://playwright.dev/'); // Test Friday
   await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright'); 
 
 });
