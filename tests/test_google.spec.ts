4554d00d4e5fc30e1661528a3c3c4e05fcbe600b
Nazariy Baranetskyy <nazariybaranetskyy@mac.mynetworksettings.com>
2025-10-03 21:03:00 -0400

    Expanded test with additional functionality

--- tests/test_google.spec.ts
+++ tests/test_google.spec.ts
@@ -2,5 +2,7 @@ import { test, expect } from '@playwright/test';
 test('check test', async ({ page }) => {
   await page.goto('https://playwright.dev/'); // Test 3
   await expect(page).toHaveTitle('Fast and reliable end-to-end testing for modern web apps | Playwright'); 
-
+  await page.waitForTimeout(5000);
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.waitForTimeout(5000);
 });
